3532c1987fb718ebc90efb838ba8e6553c2a4aaa
Jatin <Jatsharma9@gmail.com>
2020-10-02 15:42:12 +0530

    Craiglist clone with GUI

---
+++ craig/my_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+app ='my_app'
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('new_search/', views.new_search, name='new_search')
+]
